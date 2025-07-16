3db27b2beafc8c21d700d4ad56738807808ac5c7
Fatih K <sdetfatih@gmail.com>
2025-07-16 23:42:25 +0200

    De wijzigingen die ik local heb aangebracht opslaan

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,17 @@
-FROM ubuntu:20.04
+FROM node:18
 
-ARG APP_NAME
+# Set working directory
+WORKDIR /app
 
-# test arg
-RUN test -n "$APP_NAME"
-
-# install system packages
-RUN apt-get update -y
-RUN apt-get install -y \
-  vim \
-  wget \
-  curl \
-  net-tools \
-  xz-utils
-
-# install node
-WORKDIR /opt
-RUN wget https://nodejs.org/dist/v14.19.1/node-v14.19.1-linux-x64.tar.xz
-RUN tar -xvf node-v14.19.1-linux-x64.tar.xz
-ENV PATH="${PATH}:/opt/node-v14.19.1-linux-x64/bin"
-# RUN ln -s /opt/node-v14.19.1-linux-x64/bin/node /usr/bin/
-# RUn ln -s /opt/node-v14.19.1-linux-x64/bin/npm /usr/bin/
-
-# setup user
-RUN useradd -ms /bin/bash ubuntu
-USER ubuntu
-
-# install app
-RUN mkdir -p /home/ubuntu/"$APP_NAME"/"$APP_NAME"
-WORKDIR /home/ubuntu/"$APP_NAME"/"$APP_NAME"
-COPY . .
-RUN echo $PATH
+# Copy package files and install deps
+COPY package*.json ./
 RUN npm install
 
+# Copy the rest of the app
+COPY . .
+
+# Expose default React port
+EXPOSE 3000
+
+# Start the app
 CMD ["npm", "start"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
-import Login from './pages/auth/Login';
-import { Home } from './pages/home/Home';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/auth/Login'
+import { Home } from './pages/home/Home'
 
-function App() {
-
+function App () {
   return (
-    <>
-      <Router>
-        <Routes>
-          {/* <Route
-            path='/'
-            element={
-              isLoggedIn ? (hasSelectedCompany ? (<Home />) : (<Navigate to='/company' />)) : (<Login />)
-            }
-          /> */}
-          {/* <Route path='*' element={isLoggedIn ? <Home /> : <Login />} /> */}
-          <Route path="*" element={<Home />} />
-          <Route path="/app" element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          {/* <Route path="/" element={<Navigate to="/contacts" replace />} /> */}
-          {/* <Route
-            path='/'
-            element={isLoggedIn ? <Home /> : <Login />
-            // <Navigate to='/login' />
-          }
-          >
-          </Route> */}
-        </Routes>
-      </Router>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/app" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
